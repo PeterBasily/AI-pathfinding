808ff8ba5884d58ec1fae738ad84df4caaa3c860
Peter Basily <baspete49@gmail.com>
2021-10-07 21:14:53 -0400

    added some comments

--- src/sketch.js
+++ src/sketch.js
@@ -1,18 +1,17 @@
-var allGrids = [];
-var grid;
-var visitedCells = [];
-var current;
-var canvas;
-var reset;
-var startSearch = false;
-var r;
-var r2;
-var start;
-var finish;
-var forward;
-var searchType;
-var searching = false;
-var heap; 
+/*variables*/
+
+var allGrids = []; //all 50 grids generated
+var grid; //The current grid selected
+var canvas; //our canvas
+var reset; //bool used to reset all grids 
+var start; //the starting node
+var finish; //the finishing node
+var searchType; //the search type
+var searching = false; //bool used to start search
+var heap; //The binary heap we are using to store our open list
+
+
+/*Utility functions */ 
 function init() {
   heap = new MinHeap(Math.floor);
   reset = true;
@@ -30,10 +29,10 @@ function init() {
 
   }
   for (i = 0; i < 50; i++) {
-    visitedCells = [];
+    var visitedCells = [];
     grid = allGrids[i];
     var r = floor(random(0, grid.length))
-    current = grid[r];
+    var current = grid[r];
     current.visited = true;
     visitedCells.push(current);
     
@@ -101,8 +100,8 @@ function run() {
 
 
 function setStartAndFinish() {
-  r = floor(random(0, grid.length))
-  r2 = floor(random(0, grid.length))
+  var r = floor(random(0, grid.length))
+  var r2 = floor(random(0, grid.length))
   start = grid[r];
   finish = grid[r2];
   while (start.blocked || finish.blocked) {
@@ -117,29 +116,6 @@ function setStartAndFinish() {
   console.log(finish)
 
 }
-
-
-
-
-function setup() {
-  frameRate(60)
-  canvas = createCanvas(1010, 1010, P2D);
-  canvas.parent('canvas_parent');
-  canvas.elt.style.position = 'fixed';
-  canvas.style('top', 100);
-  canvas.style('left', (windowWidth - 1010) / 2);
-  //canvas.position((windowWidth - 1010) / 2 );
-  init();
-  var dropdown = document.getElementById("MazeSelect");
-  for (var i = 0; i < 50; i++) {
-    var newOption = document.createElement('option');
-    newOption.text = i;
-    newOption.value = i;
-    dropdown.options.add(newOption);
-  }
-
-};
-
 function setGrid(value) {
   grid = allGrids[value];
   for(let i = 0; i < grid.length; i++){
@@ -177,6 +153,29 @@ function runSearch(){
   
 
 }
+
+/****************SETUP & DRAW FUNCTIONS **********************/
+
+function setup() {
+  frameRate(60)
+  canvas = createCanvas(1010, 1010, P2D);
+  canvas.parent('canvas_parent');
+  canvas.elt.style.position = 'fixed';
+  canvas.style('top', 100);
+  canvas.style('left', (windowWidth - 1010) / 2);
+  //canvas.position((windowWidth - 1010) / 2 );
+  init();
+  var dropdown = document.getElementById("MazeSelect");
+  for (var i = 0; i < 50; i++) {
+    var newOption = document.createElement('option');
+    newOption.text = i;
+    newOption.value = i;
+    dropdown.options.add(newOption);
+  }
+
+};
+
+
 
 function draw() {
 
